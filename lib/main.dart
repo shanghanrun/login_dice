870f08e936b16ce53b7772b70bc9bcf538ee3116
shanghanrun <120538041+shanghanrun@users.noreply.github.com>
2023-11-12 00:43:25 +0900

    입력폼

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LogIn extends StatelessWidget {
+class LogIn extends StatefulWidget {
   const LogIn({super.key});
 
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,6 +31,47 @@ class LogIn extends StatelessWidget {
         centerTitle: true,
         leading: IconButton(icon: const Icon(Icons.menu), onPressed: () {}),
         actions: [IconButton(icon: const Icon(Icons.search), onPressed: () {})],
+      ),
+      body: Column(
+        children: [
+          const Padding(padding: EdgeInsets.only(top: 50)),
+          Center(
+            child: Image.asset('image/chef.gif', width: 170, height: 190),
+          ),
+          Form(
+            child: Theme(
+              data: ThemeData(
+                primaryColor: Colors.teal, //밑줄 색상
+                inputDecorationTheme: const InputDecorationTheme(
+                  labelStyle: TextStyle(
+                    color: Colors.teal,
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(40),
+                child: const Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Enter "dice"',
+                      ),
+                      keyboardType: TextInputType.emailAddress, //@기호있음
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Enter Password',
+                      ),
+                      keyboardType: TextInputType.text,
+                      obscureText: true, // 안보이게 함
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
